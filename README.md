b9183eb6ded706271cad2a10ab84ca5b781b4c06
Ilya Ovdin <iovdin@gmail.com>
2023-03-15 11:40:36 +0200

    update to the latest version of pytorch

--- README.md
+++ README.md
@@ -1,15 +1,6 @@
 # VQ-VAE
 
 This is a lightweight (200 loc) implementation of the VQ-VAE [Neural Discrete representation learning](https://arxiv.org/pdf/1711.00937.pdf).  
-[TensorComprehensions](https://github.com/facebookresearch/TensorComprehensions) is used 
-to reduce memory required to calculate distance to embeddings.  
-
-A sensitivity term is introduced, to make all embeddings used. A sensitivity is substracted from a distance to embedding that is not used for some time. Before finding minimum distance.
-
-## requirements
- - Python 3.6 
- - PyTorch 0.3
- - TensorComprehensions
 
 # Training
 By default it trains on cifar10
@@ -18,8 +9,6 @@ By default it trains on cifar10
 python vq-vae-img.py
 ```
 Edit hyperparams, paths in source code to train on ImageNet  
-
-I used https://lera.ai to track model learning progress. It is off by default, use ``--lera`` to enable it.
 
 ### ImageNet reconstruction after 40k iterations (K=512, D=128)
 ![ImageNet](./imagenet.jpg)
--- vq-vae-img.py
+++ vq-vae-img.py
@@ -5,24 +5,17 @@ import torch.optim as optim
 import torchvision.datasets
 import torchvision.transforms as transforms
 import torchvision.utils
-from torch.autograd import Variable as V
 from torch.utils.data import DataLoader
-import tensor_comprehensions as tc
-import numpy
+from einops import rearrange, repeat
+from tqdm import tqdm
+from PIL import Image
+import numpy as np
 
-import lera
-import argparse
 import utils
 import os
 
-parser = argparse.ArgumentParser(description='PyTorch VQ-VAE training on Cifar10 and ImageNet')
-parser.set_defaults(lera=False)
-parser.set_defaults(no_cuda=False)
-parser.add_argument('--lera', dest='lera', action='store_true', help='Should i send training stats to https://learning-rates.com')
-parser.add_argument('--no-cuda', dest='no_cuda', action='store_true', help='Run on CPU')
 
-args = parser.parse_args()
-use_cuda = not args.no_cuda and torch.cuda.is_available()
+use_cuda = torch.cuda.is_available()
 
 ## Model
 
@@ -53,161 +46,98 @@ def decoder(ch):
 
 ## Dataset
 
-datasets = {}
 
-datasets['cifar10'] = torchvision.datasets.CIFAR10(root="./data/cifar10", download=True, train=True, transform=transforms.ToTensor())
+dataset = torchvision.datasets.CIFAR10(root="./data/cifar10", download=True, train=True, transform=transforms.ToTensor())
 
-imgnet_transform = transforms.Compose([
-    transforms.Resize(128),
-    transforms.CenterCrop(128),
-    transforms.ToTensor(),
-])
-
-datasets['imagenet128'] = torchvision.datasets.ImageFolder(root="./data/imagenet/train", transform=imgnet_transform)
 
 ## Hyperparameters
 
 batch_size = 128
 lr = 2e-4
 # categorical dimention
-K = 64
+K = 128
 # embedding size
-D = 64
+D = 128
 beta = 0.25 
-total_steps = 250000
+total_steps = 250000 * batch_size
 
-dataset = 'cifar10' 
-#dataset = 'imagenet128'
+def train():
+    step = 0
+    epochs = total_steps // len(dataset) + 1
+    tq = tqdm(range(epochs))
+    for epoch in tq:
+        for input, _ in DataLoader(dataset, batch_size=batch_size, pin_memory=use_cuda, num_workers=2, shuffle=True, drop_last=True):
+            if use_cuda:
+                input = input.cuda()
 
-field_size = 8 if dataset== 'cifar10' else 32
+            step += batch_size 
+            if step > total_steps:
+                return
 
-## TensorComprehension module to calculate distance between embedding and encoder output
-# it saves a lot of memory
+            ze = enc(input)
+            field_size = ze.shape[-1]
 
-cross_dist_lang = """
-def cross_dist(float(N,C,H,W) X, float(K, C) EMB) -> (dist) {
-   dist(n, k, h, w) +=! pow(fabs(X(n, c, h, w) - EMB(k, c)), 2.0)
-}
-"""
-cross_dist_cuda = tc.define(cross_dist_lang, name="cross_dist")
+            # calculate distance to embeddings
+            index = (torch.cdist(
+                rearrange(ze.detach(), 'B D W H -> B (W H) D'), 
+                repeat(embeddings, 'K D -> B K D', B = batch_size))
+                .min(-1)[1])
+            sz = index.size()
 
-autotuned = dict()
+            # running exponential average
+            emb = embeddings.detach() * 0.99 + embeddings.clone() * 0.01
 
-def autotune(batch_size, K, D, field_size):
-    global autotuned
-    cache_autotune = ".autotune_{0:02d}_{1:02d}_{2:02d}_{3:02d}".format(batch_size, K, D, field_size)
-    if autotuned.get(cache_autotune, None) is not None:
-        return cache_autotune
-    if not os.path.exists(cache_autotune + ".options") and use_cuda:
-        input = torch.randn(batch_size, D, field_size, field_size).cuda()
-        emb = torch.randn(K, D).cuda()
-        cross_dist_cuda.autotune(input, emb , cache=cache_autotune, generations=10)
-        autotuned[cache_autotune] = True
-    return cache_autotune
+            zq = rearrange(emb[index.view(-1)], '(B W H) D -> B D W H', B=batch_size, W=field_size, H=field_size)
 
-def min_dist(input, emb):
-    if use_cuda:
-        cache_autotune = autotune(input.size(0), emb.size(0), emb.size(1), input.size(2))
-        return cross_dist_cuda(input, emb, cache=cache_autotune).sub(V(sensitivity.view(1, K, 1, 1))).min(1)[1]
+            emb_loss = (zq - ze.detach()).pow(2).sum(1).mean() + 1e-2 * embeddings.pow(2).mean()
 
-    # cpu version
-    return (input.permute(0, 2, 3, 1) # [batch_size, w, h, D]
-             .unsqueeze(-2)           # [batch_size, w, h, 1, D]
-             .sub(emb)                # [batch_size, w, h, K, D]
-             .pow(2).sum(-1)          # [batch_size, w, h, K]
-             .sub(V(sensitivity.view(1, 1, 1, K)))
-             .min(-1)[1])
+            # detach zq so it won't backprop to embeddings with recon loss
+            zq = zq.detach()#, requires_grad=True
+            zq.requires_grad_(True)
 
-emb_history = []
+            output = dec(zq)
 
-def train(epoch, step):
-    #lera.log('epoch', epoch)
-    epoch += 1
+            commit_loss = beta * (ze - zq.detach()).pow(2).sum(1).mean()
+            recon_loss = F.mse_loss(output, input)
 
-    for input, _ in DataLoader(datasets[dataset], batch_size=batch_size, pin_memory=use_cuda, num_workers=2, shuffle=True, drop_last=True):
-        if use_cuda:
-            input = input.cuda()
+            optimizer.zero_grad()
 
-        step += 1
+            commit_loss.backward(retain_graph=True)
+            emb_loss.backward()
+            recon_loss.backward()
 
-        ze = enc(V(input))
+            # pass data term gradient from decoder to encoder
+            ze.backward(zq.grad)
 
-        index = min_dist(V(ze.data), embeddings)
-        sz = index.size()
+            optimizer.step()
 
-        zq = (embeddings[index.view(-1)]       # [batch_size * x * x, D] containing vectors from embeddings
-                .view(sz[0], sz[1], sz[2], D)  # [batch_size, x, x, D] 
-                .permute(0, 3, 1, 2))          # [batch_size, D, x, x]
+            emb_count[index.data.view(-1)] = 1
+            emb_count.sub_(0.01).clamp_(min=0)
+            unique_embeddings = emb_count.gt(0).sum().item()
 
-        emb_loss = (zq - V(ze.data)).pow(2).sum(1).mean() + 1e-2 * embeddings.pow(2).mean()
+            tq.set_postfix(
+                recon_loss = recon_loss.item(),
+                commit_loss = commit_loss.item(),
+                unique_embeddings = unique_embeddings,
+                step=step)
 
-        # detach zq so it won't backprop to embeddings with recon loss
-        zq = V(zq.data, requires_grad=True)
 
-        output = dec(zq)
+        input = input.cpu()[0:8,:,:,:]
+        w = input.size(-1)
+        output = output.data.cpu()[0:8,:,:,:]
+        result = (torch.stack([input, output])           # [2, 8, 3, w, w]
+                    .transpose(0, 1).contiguous()        # [8, 2, 3, w, w]
+                    .view(4, 4, 3, w, w)                 # [4, 4, 3, w, w]
+                    .permute(0, 3, 1, 4, 2).contiguous() # [4, w, 4, w, 3]
+                    .view(w * 4, w * 4, 3))              # [w * 4, w * 4, 3]
 
-        commit_loss = beta * (ze - V(zq.data)).pow(2).sum(1).mean()
-        recon_loss = F.mse_loss(output, V(input))
-
-        optimizer.zero_grad()
-
-        commit_loss.backward(retain_graph=True)
-        emb_loss.backward()
-        recon_loss.backward()
-
-        # pass data term gradient from decoder to encoder
-        ze.backward(zq.grad)
-
-        optimizer.step()
-
-        emb_count[index.data.view(-1)] = 1
-        emb_count.sub_(0.01).clamp_(min=0)
-        unique_embeddings = emb_count.gt(0).sum()
-
-        sensitivity.add_(emb_loss.data[0] * (K - unique_embeddings) / K)
-        sensitivity[emb_count.gt(0)] = 0
-
-        lera.log({ 
-            'recon_loss': recon_loss.data[0],
-            'commit_loss': commit_loss.data[0],
-            'unique_embeddings': emb_count.gt(0).sum(),
-            }, console=True)
-
-        # make comparison image
-        if lera.every(seconds=60):
-            input = input.cpu()[0:8,:,:,:]
-            w = input.size(-1)
-            output = output.data.cpu()[0:8,:,:,:]
-            result = (torch.stack([input, output])           # [2, 8, 3, w, w]
-                        .transpose(0, 1).contiguous()        # [8, 2, 3, w, w]
-                        .view(4, 4, 3, w, w)                 # [4, 4, 3, w, w]
-                        .permute(0, 3, 1, 4, 2).contiguous() # [4, w, 4, w, 3]
-                        .view(w * 4, w * 4, 3))              # [w * 4, w * 4, 3]
-            lera.log_image('reconstruction', result.numpy(), clip=(0, 1))
-
-    # continue training
-    if step < total_steps:
-        train(epoch, step)
+        Image.fromarray( np.uint8((result * 255).numpy())).save("samples/{:03d}.jpg".format(epoch))
 
 ## Train
-
-lera.enabled(args.lera)
-lera.log_hyperparams({
-    'title' : "VQ-VAE",
-    'dataset': dataset,
-    'batch_size': batch_size,
-    'K': K,
-    'lr': lr,
-    'D': D,
-    'beta': beta,
-    'total_steps' : total_steps
-    })
-lera.log_file(__file__)
 
 enc = encoder(D) 
 dec = decoder(D)
 embeddings = torch.randn(K, D).div(D)
-sensitivity = torch.zeros(K)
 
 # calculate number of embedding vectors used
 emb_count = torch.zeros(K)
@@ -217,13 +147,12 @@ if use_cuda:
     dec = dec.cuda()
     embeddings = embeddings.cuda()
     emb_count = emb_count.cuda()
-    sensitivity = sensitivity.cuda()
 
 embeddings = nn.Parameter(embeddings, requires_grad=True)
-optimizer = optim.Adam([ 
+optimizer = optim.AdamW([ 
     { 'params' : enc.parameters() },
     { 'params' : dec.parameters() },
     { 'params' : embeddings }
     ], lr=lr)
 
-train(1, 1)
+train()
